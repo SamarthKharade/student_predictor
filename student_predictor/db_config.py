144e28f05a52bdb02aa67d713abacf2564e69a06
Samarth Kharade <110157155+SamarthKharade@users.noreply.github.com>
2025-04-15 03:10:27 +0530

    Update db_config.py

--- student_predictor/db_config.py
+++ student_predictor/db_config.py
@@ -3,9 +3,9 @@ import mysql.connector
 
 def get_connection():
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-      
+        host=os.getenv("DB_HOST"),        # Should resolve to Railway's DB host
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
+        database=os.getenv("DB_NAME"),
+        port=3306
     )
