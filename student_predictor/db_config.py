80643382f23ec4a5ddf763bcc4d18fbb607bf9ff
Samarth Kharade <110157155+SamarthKharade@users.noreply.github.com>
2025-04-15 02:27:14 +0530

    Update db_config.py

--- student_predictor/db_config.py
+++ student_predictor/db_config.py
@@ -4,6 +4,7 @@ import mysql.connector
 def get_connection():
     return mysql.connector.connect(
         host=os.getenv("DB_HOST"),
+          port=3306
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME")
