c3792bd65f0cbae1a434781e7c99d2a4cabc193d
Samarth Kharade <110157155+SamarthKharade@users.noreply.github.com>
2025-04-17 18:06:56 +0530

    Update app.py

--- student_predictor/app.py
+++ student_predictor/app.py
@@ -39,4 +39,5 @@ def predict():
                            hours=hours, attendance=attendance, internal=internal)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
